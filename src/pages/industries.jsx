4fac1c2661312f706faa8678de60d4019d96f38a
Muhammad Qasim <dev.mhdqasim@gmail.com>
2025-04-10 10:25:54 +0500

    industries page updated to v1

--- src/pages/industries.jsx
+++ src/pages/industries.jsx
@@ -39,12 +39,15 @@ const Index = () => {
           subTitle={`Every industry has unique challenges, and the right technology can be a game-changer. We provide tailored software solutions designed to enhance efficiency, automate critical processes, and drive business growth. Whether youre optimizing supply chains, managing properties, delivering education, or providing exceptional concierge services, our solutions are built to streamline operations and elevate performance.`}>
           <div className='mb-30'>
             <H6 className='w-full md:w-4/6 mb-32 md:mb-52'>With expertise across multiple industries, we develop, integrate, and maintain scalable, future-proof software that adapts to your needs—whether through prebuilt platforms, custom development, or seamless integrations. Explore how our solutions empower businesses like yours:</H6>
-            <section className='flex flex-col items-stretch gap-16 flex-wrap xl:flex-nowrap ml-12 md:ml-32'>
+            <section className='flex flex-col items-stretch gap-16 flex-wrap xl:flex-nowrap'>
               {industryItems?.map((item, _index) => (
-                <div className='mb-20 md:mb-32'>
-                  <H4 className={'!font-semibold mb-24'}><span className='inline-block text-[#E95018] mr-6'><img className='w-18 h-18' src="/assets/tick-icon.svg" alt="" /></span> {item?.title}</H4>
-                  <H6>Streamline your <span className='lowercase'>{item?.title}</span> operations with our AI-powered automation solutions. We specialize in optimizing supply chains, reducing costs, and improving efficiency.</H6>
-                </div>
+                <section className='bg-[#121212] border border-[#282828] flex flex-col lg:flex-row items-center justify-between gap-36 md:gap-99 px-24 md:px-72 pt-36 pb-70'>
+                  <div>
+                    <H3 className={'!font-semibold mb-24'}>{item?.title}</H3>
+                    <H6>Streamline your <span className='lowercase'>{item?.title}</span> operations with our AI-powered automation solutions. We specialize in optimizing supply chains, reducing costs, and improving efficiency.</H6>
+                  </div>
+                  <img className='w-full md:w-full max-h-[360px] object-contain px-20 md:pr-0' src={`/assets/globe${item?.imgUrl}`} alt="" />
+                </section>
               ))}
             </section>
           </div>
